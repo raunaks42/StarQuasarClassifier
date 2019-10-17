901a8640ec3e5302d58517f1ccec18d56dd035e2
Raunak Sengupta <42582079+raunaks42@users.noreply.github.com>
2019-10-17 18:02:37 +0530

    optimised more, cleaned, added comments

--- knn.py
+++ knn.py
@@ -1,132 +1,85 @@
-import csv
-import random
-import math
-import operator
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
-import os
+import operator
 import time
-import numpy as np
-
-'''
-def loadDataset(filename, split, trainingSet=[], testSet=[]):
-	with open(filename, 'r') as csvfile:
-		lines = csv.reader(csvfile)
-
-		dataset = (pd.DataFrame(list(lines)))[:][1:]    #remove column labels
-		dataset.drop(dataset.columns[0:2],axis=1).drop(dataset.columns[13],axis=1).drop(dataset.columns[15:],axis=1)
-		
-		sc_X = MinMaxScaler(feature_range=(0, 1))
-		dataset = sc_X.fit_transform(dataset)
-		# print("\n\n\n",dataset[1],"\n\n\n")
-		for x in range(len(dataset) - 1):
-			# print(dataset[x],"\n")
-			for y in range(12): #to use all the data values upto the 37th column in the Dataset csv
-				dataset[x][y] = float(dataset[x][y])
-			if random.random() < split:
-				trainingSet.append(dataset[x])	#the set containing the data points that we'll use to train the algorithm
-			else:
-				testSet.append(dataset[x])	#the set containing the data points that we'll use to test the algorithm
-'''
+import os
+import warnings
+warnings.filterwarnings('ignore')
 
 def loadDataset(filename):
-	#with open(filename, 'r') as csvfile:
-		#lines = csv.reader(csvfile)
-        
-		#dataset = ( pd.DataFrame(list(lines)) )[:][1:]    #remove column labels (first row)
 		dataset=pd.read_csv(filename)
+		
 		if len(dataset.columns)==38:
 		    dataset=dataset.drop(dataset.columns[15:],axis=1) #drop all cols after class column
 		    dataset=dataset.drop(dataset.columns[13],axis=1) #drop fuv
-		else:
-		    dataset=dataset.drop(dataset.columns[14:],axis=1)
-		
-		y=dataset['class']    
+		else:   #31 cols, ie, all catalog 4 csvs
+		    dataset=dataset.drop(dataset.columns[14:],axis=1) #drop all after class
+		   
 		dataset=dataset.drop(dataset.columns[0:2],axis=1) #drop id cols
+		y=dataset['class']
 		x=dataset.drop(dataset.columns[11],axis=1) #drop class col (after previous drops, its col index is 11
 		
 		sc = MinMaxScaler(feature_range=(0, 1))
-		x = sc.fit_transform(x)
-		y = y.to_numpy()
+		x = sc.fit_transform(x) #scale x vals
+		y = y.to_numpy()    #convert y to numpy array
 		
 		return x,y
-		#for x in range(len(dataset)):
-		    #Set.append(dataset[x])
 
 def getNeighbors(trainingSetx, trainingSety, testInstance, k):
-	#getting the nearest neighbours
-	#testInstance contains the xth (the entire row of values {list of len 38}) value of the testDataSet, so we'll be getting the nearest neighbours of that data point(list), in the test data set, wrt to the trainDataSet
 	distances = []
 	for x in range(len(trainingSetx)):
-		dist = manhattanDistance(testInstance, trainingSetx[x])
-		distances.append( ( (trainingSetx[x],trainingSety[x]) , dist ) )
-	distances.sort(key=operator.itemgetter(1))
-	neighbors = []
-	for x in range(k):
-		neighbors.append(distances[x][0])
+		distances.append( ( trainingSety[x] , manhattanDistance(testInstance,trainingSetx[x]) ) ) # tuple ( training example y, distance from test example ) appended in distances
+	
+	distances.sort(key=operator.itemgetter(1))  #sort by distance ascending
+	neighbors=[x[0] for x in distances[:k]]    #for best k, remove distance values, ie, append training example y to neighbours
+	
 	return neighbors
 
 def manhattanDistance(instance1, instance2):
-	#instance 1: the testInstance 
-	#instance 2: the xth value of the training data set 
-	#length 3: the total number of columns-1: 38
-	
-	distance=np.sum(np.absolute(instance1-instance2))
-	return distance
-	#return math.sqrt(distance)
+	return np.sum(np.absolute(instance1-instance2)) #element wise subtraction, element wise absolute, sum of elements
 
 def getResponse(neighbors):
-	classVotes = {}
+	classVotes = {0:0,1:0}  #votes of class 0 and class 1
 	for x in range(len(neighbors)):
-		response = neighbors[x][1]
-		if response in classVotes:
-			classVotes[response] += 1
-		else:
-			classVotes[response] = 1
-	sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
-	return sortedVotes[0][0]
-'''
-def getAccuracy(testSety, predictions):
-	correct = 0
-	for x in range(len(testSety)):
-		if testSety[x] == predictions[x]:
-			correct += 1
-	return (correct/float(len(testSety))) * 100.0
-'''	
+		response = neighbors[x]  #y value of neighbor training example
+		classVotes[response] += 1
+	return int(classVotes[1]>classVotes[0]) #1 if more votes from label 1, else 0
+		
 def kNN(Setx, Sety, trainingSetx, trainingSety, k):
     predictions=[]
     for x in range(len(Setx)):	#runs the number of times the amount of values in the test data set
-        neighbors = getNeighbors(trainingSetx, trainingSety, Setx[x], k)	#trainingset->list of lists, testset[x] -> 1 list, basically the entire row 
-        result = getResponse(neighbors)
+        neighbors = getNeighbors(trainingSetx, trainingSety, Setx[x], k)    #returns all nearest y values
+        result = getResponse(neighbors) #returns majority vote
         predictions.append(result)
-        #print('> predicted=' + repr(result) + ', actual=' + repr(testSet[x][-1]))
-    #accuracy = getAccuracy(Sety, predictions)
-    fscore=f1_score(Sety.tolist(),predictions)*100.0
-    return fscore
+        
+    return f1_score(Sety.tolist(),predictions)*100.0    #sety.tolist() converts numpy array to list
 
 def main():
-    # prepare data
-    direc=os.fsencode('.')
-    trainingSetx,trainingSety=loadDataset('cat1.csv')
+    direc=os.fsencode('.')  #encode current directory
+    
+    trainingSetx,trainingSety=loadDataset('cat1.csv')   #load training data
     print('Train file: cat1.csv')
     print ('Train set size: ' + repr(len(trainingSetx)),'\n\n')
+    
     inittime=time.time()
-    #filenames=['cat1_r1.csv','cat1_r2.csv','cat1_r3.csv']
-    for File in os.listdir(direc):  #each file in directory
-        filename=os.fsdecode(File)  #get filename
-    #for filename in filenames:
+    
+    filenames=['cat1_r1.csv','cat1_r2.csv','cat1_r3.csv']
+    for filename in filenames:
+    #for File in os.listdir(direc):  #each file in directory
+        #filename=os.fsdecode(File)  #get filename
         if filename.endswith('.csv'):
-            testSetx,testSety=loadDataset(filename)
-            #split = 0.67
+            testSetx,testSety=loadDataset(filename) #load test data
             print('Test file: ',filename)
             print ('Test set size: ' + repr(len(testSetx)))
-            cvSetx, reportSetx, cvSety, reportSety = train_test_split(testSetx,testSety, test_size = 0.5, random_state = 0)
+            
+            cvSetx, reportSetx, cvSety, reportSety = train_test_split(testSetx,testSety, test_size = 0.5, random_state = 0) #split half into set for finding best k and set for reporting final fscore
             
             bestk=1
             bestacc=0
-            for k in range(1,32,2):
+            for k in range(1,32,2): #k=1,3,...,31
                 accuracy = kNN(cvSetx, cvSety, trainingSetx, trainingSety, k)
                 if accuracy>bestacc:
                     bestk=k
@@ -134,8 +87,9 @@ def main():
                 print("k=",k,"\tf1 score=",accuracy,'%')
             
             print("Best k=", bestk,"\n")
-            accuracy = kNN(reportSetx,reportSety, trainingSetx, trainingSety, bestk)
+            accuracy = kNN(reportSetx,reportSety, trainingSetx, trainingSety, bestk)    #report final fscore for best k
             print('Reported f1 score: ' + repr(accuracy) + '%\n\n')
-            print('Time elapsed:', time.time()-inittime)
+            
+    print('Total time elapsed:', time.time()-inittime)
 	    
 main()
